4bf16cc09c96098a40c7d24a41311188484569d5
Tiberiu Matei <matei.tiberiu02@gmail.com>
2023-01-22 20:08:24 +0000

    API Deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-    res.send({ message: 'Elvana!' });
+    res.send({ message: 'Marre!' });
 });
 
 const port = process.env.port || 3333;
