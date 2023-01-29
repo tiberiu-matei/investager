e200836bc9df75e2c2749180cf394bd7bcc33b71
Tiberiu Matei <matei.tiberiu02@gmail.com>
2023-01-29 21:03:21 +0000

    Add API dependencies to image

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,10 +11,10 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-    res.send({ message: 'Mules!' });
+    res.send({ message: 'Istre!' });
 });
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/api`);
 });
--- libs/common/src/lib/common.ts
+++ libs/common/src/lib/common.ts
@@ -3,5 +3,5 @@ export function common(): string {
 }
 
 export function elvana(): string {
-    return 'Mdon2';
+    return 'Ex';
 }
